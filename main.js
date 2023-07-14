a20cf3ce4d810c862628d9b5bbddeea16024ff1e
SeraHuang1 <94316138+SeraHuang1@users.noreply.github.com>
2023-07-14 09:56:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -34,8 +34,7 @@ function draw(){
     image(video, 0, 0, 300, 300);
     fill(255, 0, 0);
     stroke(255, 0, 0);
-    circle(noseX, noseY, 20);
-    image(clown_nose, noseX, noseY, 30, 30);
+    image(clown_nose, noseX - 10, noseY - 10, 20, 20);
 }
 
 function take_snapshot(){
